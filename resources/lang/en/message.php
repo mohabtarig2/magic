3ed2ef038c1a291f5e0916879d8f1d43f7ca0106
mohabtarig2 <74310555+mohabtarig2@users.noreply.github.com>
2022-02-19 22:57:22 +0400

    first commit

---
+++ resources/lang/en/message.php
@@ -0,0 +1,84 @@
+<?php
+
+return [
+    'magic_hand' => 'Magic Hand ',
+    'Find_out' => ' Find out ',
+    'Call_Now' => 'Call_Now',
+    'magic_hand_slug' => 'Ac maintenance & installation',
+    'Home' => 'Home',
+    'How' => 'Our Mindset ',
+    'About' => 'About Us',
+    'OurServices' => 'Our Services',
+    'WhyUs' => ' Why Us',
+    'contact' => 'Contact Us',
+    "24Hours"=>"24 Hours",
+    "weclome"=>" welcome in Magic Hand ",
+    "Location"=>" Our Location ",
+    "Abudhabi"=>"  Abudhabi ",
+    "Online_Support"=>"Online Support",
+    "Call_Us"=>"Call us",
+    "Years_Experience"=>"Years Experience",
+    "aboutP"=>'Magic Hand Company, under Emirati management, is one of the best companies for
+     the maintenance of air conditioners in Abu Dhabi. It provides quick solutions and the highest
+      quality. The company also provides its services in Dubai, Ajman and Sharjah. It has a team of work at the highest level and
+    provides many services efficiently and in a short time and is characterized by the speed of responding to requests',
+    
+    "safty1"=>'Securing your air conditioner in the best way',
+    "safty2"=>'Securing & Safty',
+    "safty3"=>'Your air conditioners are in safe hands, our solutions are fast and high quality',
+
+    
+
+    "maintenance1"=>'Continuous maintenance makes you enjoy the quality of air conditioning',
+    "maintenance2"=>'maintenance & Cleaning ',
+    "maintenance3"=>'Regular maintenance and cleaning of the air conditioner that dispenses with the surprises of its failure',
+  
+    "team1"=>'The best team work just for you',
+    "team2"=>'staff  ',
+    "team3"=>'Professional staff ready to meet your requests at any time',
+
+
+    "mindsetP"=>'We are very happy to provide our customer services and achieve the highest customer satisfaction ',
+
+    'goals'=>"Our Goals",
+    'goalsP'=>"At Hand Magic, we strive to improve the installation and maintenance of air conditioners and pay attention to all the details of the customer's requirements",
+    
+    'Vision'=>"Our Vision",
+    'visionP'=>"To be the best magic hand in the UAE, the highest quality, the lowest cost and the fastest achievement",
+
+
+    'Mission'=>'Our Mision',
+    'MissionP'=>'Magic Hand offers its services to thousands of customers.
+     The happiness of our customers is the essence of our work and our urge to achieve the best customer service.',
+
+     "Why_Choose_Us"=>'Why Choose Us ',
+     "Best_Services "=>'Best Services',
+
+    'qTitle1'=>'A complete service starts from the moment you contact us',
+    'q1'=>'Our team is dedicated to providing a comprehensive service
+     24 hours a day, 7 days a week by expert masters fully trained to implement all client requests',
+
+
+    'qTitle2'=>'A track record of excellence',
+    'q2'=>'Today, Magic Hand receives more than 3000 requests from customers, creating greater confidence between them and new customers',
+
+
+    
+    'qTitle3'=>'The latest technology and equipment',
+    'q3'=>'Over the past years amazing progress has been made in the field of conditioning, Magic Hand continues to bring in modern technologies and train experts to deliver the highest quality',
+
+    'qTitle4'=>'We have a passion for serving our customers',
+    'q4'=>'We do not evaluate our success on financial metrics, but on the satisfaction of our customers. We strongly believe that customer satisfaction is the best marketing tool, so our promises remain the best tool for getting more customers',
+
+    'Quality'=>'Quality & Best Equipments',
+    'Support'=>'24/7 Support & Review',
+    'Customer'=>'Customer satisfaction',
+
+      
+    'installation'=>'Installation',
+    'supply'=>'Supply',
+    'maintenance'=>'Maintenance',
+    'Cleaning'=>'Cleaning',
+    ]
+
+?>
